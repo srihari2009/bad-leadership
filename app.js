6f194ceee41bc19492226df9a2ebda4578996f23
srihari2009 <83279752+srihari2009@users.noreply.github.com>
2021-10-06 18:52:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+document.getElementById( "button1" ).onclick = () => {
+    document.getElementById( "button1" ).style.backgroundColor = "red";
+    }
+document.getElementById( "button2" ).onclick = () => {
+    document.getElementById( "button2" ).style.backgroundColor = "green";
+    }    
+document.getElementById( "button3" ).onclick = () => {
+    document.getElementById( "button3" ).style.backgroundColor = "blue";
+    }    
+document.getElementById( "button4" ).onclick = () => {
+    document.getElementById( "button4" ).style.backgroundColor = "yellow";
+    }    
